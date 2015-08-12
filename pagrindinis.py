d44be1db54d78a6e31233e3eddc8123a5cd5afe8
Edzemike <edzemike@gmail.com>
2015-08-12 16:25:58 +0300

    papildomai padarytas sujungimas su kitu failu, vietoj load_string panaudojant load_file.

--- pagrindinis.py
+++ pagrindinis.py
@@ -3,53 +3,18 @@ __author__ = 'Edita'
 from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+import kivy.uix.image
 from kivy.lang import Builder
 
-Builder.load_string("""
+Builder.load_file("pagr.kv")
 
-<pirmas>:
-    canvas:
-        Color:
-            rgba: (1, 1, 1, .8)
-        Rectangle:
-            size: self.size
-            pos: self.pos
-    size_hint: 1, 1 #procentai: 1 - 100%, .5 - 50%
-    pos_hint: {'center_x': .5, 'center_y': .5}
-    do_default_tab: False
-
-    TabbedPanelItem:
-        canvas:
-            Color:
-                rgba: (1, 1, 1, .6)
-            Rectangle:
-                size: self.size
-                pos: self.pos
-        text: 'Value'
-        RstDocument:
-            text:
-                "Calculates the resistance value according to color-code."
-    TabbedPanelItem:
-        canvas:
-            Color:
-                rgba: (1, 1, 1, .6)
-            Rectangle:
-                size: self.size
-                pos: self.pos
-        text: 'Colour'
-        RstDocument:
-            text:
-                "Calculates the color-code according to the value of the resistance."
-""")
-
-
-class pirmas(TabbedPanel):
+class Tabbedpanel (TabbedPanel):
     pass
 
-
-class TabbedPanelApp(App):
+class MainApp(App):
     def build(self):
-        return pirmas()
+        return Tabbedpanel()
 
 if __name__ == '__main__':
-    TabbedPanelApp().run()
+    MainApp().run()
